5aeb25aa1084bf32a6dc44e00c925d06f997bee2
EvilWolve <derboesewolfi@googlemail.com>
2018-04-17 23:51:13 +0300

    Added basic building UI.

---
+++ Assets/Scripts/UI/BuildingEntry.cs
@@ -0,0 +1,24 @@
+﻿using Configuration.Building;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+namespace UI
+{
+    public class BuildingEntry : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI title;
+        [SerializeField] Image icon;
+
+        BuildingConfiguration config;
+
+        public void Init(BuildingConfiguration config)
+        {
+            this.config = config;
+
+            this.title.text = this.config.buildingName;
+            this.icon.sprite = this.config.icon;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/BuildingMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Configuration.Building;
+using UnityEngine;
+using Utilities;
+
+namespace UI
+{
+	public class BuildingMenu : MonoBehaviour
+	{
+		[SerializeField] Transform gridRoot;
+		[SerializeField] GameObject entryPrefab;
+
+		IBuildingConfigurationService buildingConfigurationService;
+
+		readonly List<BuildingEntry> entries = new List<BuildingEntry>();
+
+		BuildingType selectedBuildingType;
+		
+		// TODO: Add functionality to tabs to switch building types
+
+		void Awake()
+		{
+			this.buildingConfigurationService = ServiceLocator.Instance.GetService<IBuildingConfigurationService>();
+			
+			this.selectedBuildingType = BuildingType.COMMON;
+			
+			this.InitialiseBuildings();
+		}
+
+		void InitialiseBuildings()
+		{
+			List<BuildingConfiguration> buildingsOfType = this.buildingConfigurationService.GetBuildingsOfType(this.selectedBuildingType);
+
+			for (int i = 0; i < buildingsOfType.Count; i++)
+			{
+				BuildingEntry entry;
+				if (i < this.entries.Count)
+				{
+					entry = this.entries[i];
+				}
+				else
+				{
+					GameObject entryObj = Object.Instantiate(this.entryPrefab, this.gridRoot);
+					entry = entryObj.GetComponent<BuildingEntry>();
+					
+					this.entries.Add(entry);
+				}
+				
+				entry.gameObject.SetActive(true);
+				entry.Init(buildingsOfType[i]);
+			}
+
+			for (int i = buildingsOfType.Count; i < this.entries.Count; i++)
+			{
+				this.entries[i].gameObject.SetActive(false);
+			}
+		}
+	}
+}
