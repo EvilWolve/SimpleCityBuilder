7099d83a753461fdd98e300a56cc0098e5f41caa
EvilWolve <derboesewolfi@googlemail.com>
2018-04-18 22:43:57 +0300

    Added UICamera

---
+++ Assets/Scripts/Utilities/UI/UICamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities.UI
+{
+    public class UICamera : MonoBehaviour
+    {
+        public static UICamera Current;
+
+        void Awake()
+        {
+            if (UICamera.Current != null)
+            {
+                Object.Destroy(this.gameObject);
+                return;
+            }
+
+            UICamera.Current = this;
+        }
+    }
+}
