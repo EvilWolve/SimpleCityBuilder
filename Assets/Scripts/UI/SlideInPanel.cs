528c61eea559528b4ced982eb61489c0f835a96a
EvilWolve <derboesewolfi@googlemail.com>
2018-04-19 21:49:24 +0300

    Added sliding panel to hide the building menu.

---
+++ Assets/Scripts/UI/SlideInPanel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class SlideInPanel : MonoBehaviour
+    {
+        [SerializeField] Animator slideInAnimator;
+
+        bool isVisible = true;
+
+        void Awake()
+        {
+            this.isVisible = true;
+            this.RefreshAnimations();
+        }
+
+        public void ToggleVisibility()
+        {
+            this.isVisible = !this.isVisible;
+            
+            this.RefreshAnimations();
+        }
+
+        void RefreshAnimations()
+        {
+            this.slideInAnimator.SetBool("IsVisible", this.isVisible);
+        }
+    }
+}
